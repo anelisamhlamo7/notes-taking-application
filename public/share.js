a0dd80b85a37be06e566c05d08c2d0c8263ad3c5
anelisa <anelisamhlamo7@gmail.com>
2024-10-09 16:48:45 +0200

    moved files around

--- public/share.js
+++ public/share.js
@@ -1,20 +1,4 @@
-// function shareViaEmail() {
-//     const content = quill.root.innerHTML; // Get content from the editor
 
-//     // Remove HTML tags for email body
-//     const plainTextContent = content.replace(/<\/?[^>]+(>|$)/g, "").trim();
-//     const subject = encodeURIComponent("Shared Note");
-//     const body = encodeURIComponent(plainTextContent);
-
-//     // Ensure mailto link format is correct
-//     const mailtoLink = `mailto:?subject=${subject}&body=${body}`;
-
-//     // Debugging output
-//     console.log("Mailto link:", mailtoLink);
-
-//     // Open the default email client
-//     window.location.href = mailtoLink;
-// }
 
 function shareViaEmail() {
     const content = quill.root.innerHTML; // Get content from the Quill editor
@@ -23,7 +7,6 @@ function shareViaEmail() {
     window.location.href = `mailto:?subject=${subject}&body=${body}`;
   }
   
-
 
 
 function shareViaWhatsApp() {
