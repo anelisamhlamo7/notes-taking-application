ec60c3c9f40c7f619cf687cf402dbd0a1db2ac9d
Anelisa Mhlamo <anelisamhlamo7@gmail.com>
2024-11-21 13:31:28 +0200

    fixed noteRoutes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const noteRoutes = require('./backend/routes/notes');
+const noteRoutes = require('./backend/models/notes');
 const path = require('path');
 
 app.use(express.static('public'));
